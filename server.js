a9e52f85b40bab97d391b61b940c7135101bc988
Jasim <jasimtaqi@gmail.com>
2017-03-19 13:28:51 -0700

    handle bad requests

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var app = express();
 
 app.set('json spaces', 3);
 
-mongoose.connect('mongodb://localhost/search');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 app.use('/api', apiRouter);
+
+app.use(function(req, res) { // handle faulty requests
+  res.status(400).json({ error: "Hmmm... Couldn't recognize that URL" });
+});
 
 app.listen(process.env.PORT || 5000);
